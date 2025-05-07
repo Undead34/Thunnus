a8dd33a33687b82e94228a1e7f63ec110d84343b
Undead34 <maizogabriel@gmail.com>
2025-05-07 12:58:49 -0400

    stable

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,20 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAvWNgfzyAhHi72c_sL-ZiI14asReyEP7o",
-  authDomain: "violet-visual.firebaseapp.com",
-  projectId: "violet-visual",
-  storageBucket: "violet-visual.firebasestorage.app",
-  messagingSenderId: "16379797973",
-  appId: "1:16379797973:web:2c8e2393e1280f78ee1d5c",
-  measurementId: "G-2SEJXV5QEL",
+  apiKey: "AIzaSyBNk_tCac_F1nloyLjieaa_pSpaiFyi5qg",
+  authDomain: "phi-sitca.firebaseapp.com",
+  projectId: "phi-sitca",
+  storageBucket: "phi-sitca.firebasestorage.app",
+  messagingSenderId: "93963990041",
+  appId: "1:93963990041:web:c1d305e05496535c3ea688"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,24 +1,40 @@
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
+import * as fs from 'fs';
+import * as path from "path";
 
 const activeApps = getApps();
-const serviceAccount = {
-  type: "service_account",
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
-};
+let serviceAccount: ServiceAccount;
+
+const serviceAccountPath = import.meta.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+
+if (serviceAccountPath) {
+  try {
+    const serviceAccountFileContent = fs.readFileSync(path.resolve(serviceAccountPath), 'utf8');
+    serviceAccount = JSON.parse(serviceAccountFileContent);
+  } catch (e) {
+    console.error(`Error al cargar o parsear el archivo de cuenta de servicio desde la ruta "${serviceAccountPath}":`, e instanceof Error ? e.message : String(e));
+  }
+}
+else {
+  serviceAccount = {
+    type: "service_account",
+    project_id: import.meta.env.FIREBASE_PROJECT_ID,
+    private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+    client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+    client_id: import.meta.env.FIREBASE_CLIENT_ID,
+    auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+    token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
+    client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  } as ServiceAccount;
+}
 
 const initApp = () => {
   console.info('Loading service account from env.')
   return initializeApp({
-    credential: cert(serviceAccount as ServiceAccount)
+    credential: cert(serviceAccount)
   })
 }
 
